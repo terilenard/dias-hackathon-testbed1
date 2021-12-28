d5f14b7c3d6153f93dd3c37f11cbacea4591866d
Teri Lenard <teri.lenard@umfst.ro>
2021-12-28 10:18:03 +0200

    Changed paths for kuksa.val service. Moved the config, executable and other credentials to specific dirs. Added service to correct path.

--- toolchain/scripts/kuksa.val.sh
+++ toolchain/scripts/kuksa.val.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# TODO: needs testing
 
 echo "Installing dependencies"
 
@@ -14,3 +17,23 @@ cd build
 cmake ..
 
 make
+
+sudo mkdir /usr/bin/kuksa.val
+
+cd build/src/
+
+sudo cp kuksa-val-server /usr/bin/kuksa.val/
+
+sudo cp config.ini CA.pem Client.key Client.pem jwt.key.pub Server.key Server.pem vss_release_2.0.json vss_release_2.1.json /etc/kuksa.val/
+
+cd ../../toolchain/services/kuksa.val-service
+
+sudo cp kuksa.val.service /usr/local/lib/systemd/system/kuksa.val.service
+
+sudo sed -i "/User=/c\User=$USER" /usr/local/lib/systemd/system/kuksa.val.service
+
+sudo sed -i "/Group=/c\Group=$USER" /usr/local/lib/systemd/system/kuksa.val.service
+
+sudo systemctl enable kuksa.val.service
+
+sudo systemctl start kuksa.val.service
