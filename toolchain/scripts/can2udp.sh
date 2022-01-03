bdc61ef8fcedbe2fbc275153cf1a578ad7549b78
Teri Lenard <teri.lenard@umfst.ro>
2022-01-03 18:43:12 +0200

    Changed service path

--- toolchain/scripts/can2udp.sh
+++ toolchain/scripts/can2udp.sh
@@ -12,9 +12,13 @@ sudo add-apt-repository -y ppa:lely/ppa
 
 sudo apt update
 
-sudo apt install -y autoconf automake libtool make g++ python3-pip liblely-io-dev
+sudo apt install -y autoconf automake libtool make g++ python3-pip liblely-io-dev liblely-co-dev
 
 cd ../../dependencies/lely-core
+
+git clone https://gitlab.com/lely_industries/lely-core.git
+
+cd lely-core
 
 autoreconf -i
 
@@ -24,15 +28,15 @@ make
 
 sudo make install
 
-cd ../../toolchain/services/can2udp-service
+cd ../../../toolchain/services/can2udp-service
 
-sudo cp can2udp.service /usr/local/lib/systemd/system/can2udp.service
+sudo cp can2udp.service /etc/systemd/system/can2udp.service
 
-sudo sed -i "/User=/c\User=$USER" /usr/local/lib/systemd/system/can2udp.service
+sudo sed -i "/User=/c\User=$USER" /etc/systemd/system/can2udp.service
 
-sudo sed -i "/Group=/c\Group=$USER" /usr/local/lib/systemd/system/can2udp.service
+sudo sed -i "/Group=/c\Group=$USER" /etc/systemd/system/can2udp.service
 
-sudo sed -i "/ExecStart=/c\ExecStart=/usr/local/bin/can2udp -D -fp $1 vcan0 $2 $3" /usr/local/lib/systemd/system/can2udp.service
+sudo sed -i "/ExecStart=/c\ExecStart=/usr/local/bin/can2udp -D -fp $1 vcan0 $2 $3" /etc/systemd/system/can2udp.service
 
 sudo systemctl enable can2udp.service
 
