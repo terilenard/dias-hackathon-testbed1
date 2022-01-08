9c0f1cb11998e25617e4c3c1127da6f0cec3460d
Teri Lenard <teri.lenard@umfst.ro>
2022-01-08 16:39:50 +0200

    Install script for grafana and influxdb

---
+++ toolchain/scripts/grafana.sh
@@ -0,0 +1,17 @@
+sudo apt-get install -y apt-transport-https
+
+sudo apt-get install -y software-properties-common wget
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+
+sudo apt-get update
+
+sudo apt-get install grafana
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable grafana-server
+
+sudo systemctl start grafana-server
