87601dad4a8c566ef17110fba38c7f328abd6a84
Teri Lenard <teri.lenard@umfst.ro>
2022-02-09 12:08:06 +0200

    Added a cangw service for incomming and outgoing CAN traffic

--- toolchain/scripts/cangw.sh
+++ toolchain/scripts/cangw.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]
-then
-    echo "Invalid arguments."
-    echo "Please give 2 CAN interfaces (e.g., can0, vcan0),
-    to set up a gateway"
-    exit 1
-fi
+#if [ $# -lt 2 ]
+#then
+#    echo "Invalid arguments."
+#    echo "Please give 2 CAN interfaces (e.g., can0, vcan0),
+#    to set up a gateway"
+#    exit 1
+#fi
 
 sudo apt install can-utils
 
 sudo modprobe can-gw
 
-sudo cangw -A -s $1 -d $2 -e
+cd ../toolchain/services/cangw-service
 
-sudo cangw -A -d $1 -s $2 -e 
+sudo cp cangw-in.service /etc/systemd/system/cangw-in.service
+
+sudo cp cangw-out.service /etc/systemd/system/cangw-out.service
+
+sudo sed -i "/User=/c\User=$USER" /etc/systemd/system/cangw-in.service
+
+sudo sed -i "/Group=/c\Group=$USER" /etc/systemd/system/cangw-in.service
+
+sudo sed -i "/User=/c\User=$USER" /etc/systemd/system/cangw-out.service
+
+sudo sed -i "/Group=/c\Group=$USER" /etc/systemd/system/cangw-out.service
+
+sudo systemctl enable cangw-in.service
+sudo systemctl enable cangw-out.service
+
+sudo systemctl start cangw-in.service
+sudo systemctl start cangw-out.service
