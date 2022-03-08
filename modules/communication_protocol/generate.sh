969aacd4063c5838b79f6825dddb8192fdc68c27
Teri Lenard <teri.lenard@umfst.ro>
2022-03-08 10:28:32 +0200

    Removed unnecesarry communication protocol

--- modules/communication_protocol/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# generate Python sources
-protoc --python_out=python/comm_core --proto_path=. proto/*
-
-# generate C sources
-protoc-c --c_out=c/ --proto_path=. proto/*
--- modules/communication_protocol/python/comm_core/__init__.py
+++
@@ -1,3 +0,0 @@
-# Publishing the Key manager's alerts
-KEYMNGR_PUBSUB_ADDRESS = "tcp://127.0.0.1:11001"
-KEYMNGR_FRESHKEY_TOPIC = "freshkey.keymngr"
