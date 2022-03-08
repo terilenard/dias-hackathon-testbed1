bd3cc94570f944fefec2603ab59e17cdce04e53f
Teri Lenard <teri.lenard@umfst.ro>
2022-03-08 10:35:33 +0200

    Removed unnecesarry old dir

--- old/modules/dias_kuksa/utils/in-vehicle/confvcan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# VirtualCAN Configuration
-# $ sudo chmod +x confvcan.sh    # should be done first
-# $ ./confvcan.sh    # then do this
-
-sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
