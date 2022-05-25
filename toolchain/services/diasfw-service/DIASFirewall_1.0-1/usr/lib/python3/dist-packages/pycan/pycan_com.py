8f81b61296f1b76b1094d271151a6897b0e81ed2
Teri Lenard <teri.lenard@umfst.ro>
2022-05-25 11:23:13 +0200

    Modified pipe writting

--- toolchain/services/diasfw-service/DIASFirewall_1.0-1/usr/lib/python3/dist-packages/pycan/pycan_com.py
+++ toolchain/services/diasfw-service/DIASFirewall_1.0-1/usr/lib/python3/dist-packages/pycan/pycan_com.py
@@ -2,14 +2,15 @@
 
 import can
 import time, os
-from pycan_msg import *
-from pyc_logger import logger
+from .pycan_msg import *
+from .pyc_logger import logger
+
 
 def setup_bus(channel):
     logger.debug("Setting up BUS: " + channel)
 
     try:
-        bus = can.interface.Bus(channel=channel, bustype='socketcan_native')
+        bus = can.interface.Bus(channel=channel, bustype='socketcan')
         return bus
 
     except Exception as ex:
@@ -20,21 +21,25 @@ def create_pipe(pipe_path):
     logger.debug("Opening PIPE: " + pipe_path)
 
     if os.path.exists(pipe_path):
-        os.remove(pipe_path)
-        os.mkfifo(pipe_path)
+        pipeout = os.open(pipe_path, os.O_WRONLY)
+        #os.remove(pipe_path)
+        #os.mkfifo(pipe_path)
     else:
         os.mkfifo(pipe_path)
+        pipeout = os.open(pipe_path, os.O_WRONLY)
 
-    pipeout = os.open(pipe_path, os.O_WRONLY)
     return pipeout
 
 def send_message_on_pipe(pipeout, msg):
 
-    sent_msg = (msg.arbitration_id).to_bytes(4,'big') + (msg.dlc).to_bytes(1,'little') + msg.data
+    #counter=counter+1
+    #print(counter)
+    #print((int(msg.timestamp*1000)).to_bytes(6,'little'))
+    sent_msg = ((int(msg.timestamp*1000)).to_bytes(6,'little')) + (msg.arbitration_id).to_bytes(4,'big') + (msg.dlc).to_bytes(1,'little') + msg.data
 
     d = os.write(pipeout, sent_msg)
     ts = time.time()
-    print(str(ts) , " Sent : " + str(d) + " bytes. Payload: " + str(sent_msg))
+    #logger.debug("{} Sent : {} bytes. Payload: {}".format(str(ts), str(d), str(sent_msg)))
 
 def send_message_on_can(bus, msg):
     bus.send(msg)
