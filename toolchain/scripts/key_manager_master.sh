0b34e98e5e7e333b0056076e41a0700084ee2742
Teri Lenard <teri.lenard@umfst.ro>
2022-05-26 11:51:15 +0200

    Initial commit for key manager master and slave scripts

---
+++ toolchain/scripts/key_manager_master.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+SOURCE_PATH=/usr/lib/python3/dist-packages/dias-ltk-stk/
+CONFIG_PATH=/etc/dias-ltk-stk/
+LOG_PATH=/var/log/dias-ltk-stk/
+SERVICE_NAME=key_manager_master.service
+
+# Directories
+sudo mkdir -p $SOURCE_PATH
+sudo chmod $USER:$USER $SOURCE_PATH
+
+sudo mkdir -p $CONFIG_PATH
+sudo chmod $USER:$USER $CONFIG_PATH
+
+sudo mkdir -p $LOG_PATH
+sudo chmod $USER:$USER $LOG_PATH
+
+# Log file
+touch $LOG_PATH/master_kmngr.log
+
+# Sources
+cd ../../modules/dias-ltk-stk/src/
+
+cp -r pytpm/ $SOURCE_PATH
+cp -r master_core/ $SOURCE_PATH
+cp -r utils/ $SOURCE_PATH
+cp slave_kmngr.py $SOURCE_PATH
+
+# Bootstrapping directories
+
+if [ ! -e $BOOSTRAP_DIR ]
+then
+    sudo mkdir -p $BOOSTRAP_DIR
+    sudo chown $USER:$USER $BOOSTRAP_DIR
+fi
+
+if [ ! -e $BOOSTRAP_DIR/MASTER_TPMCTX/ASYMKEYCTX ]
+then
+    sudo mkdir -p $BOOSTRAP_DIR/MASTER_TPMCTX/ASYMKEYCTX
+    sudo chown $USER:$USER $BOOSTRAP_DIR/MASTER_TPMCTX/ASYMKEYCTX
+fi
+
+if [ ! -e $BOOSTRAP_DIR/MASTER_TPMCTX/EXTKEYSTORE ]
+then
+    mkdir -p $BOOSTRAP_DIR/MASTER_TPMCTX/EXTKEYSTORE
+    sudo chown $USER:$USER $BOOSTRAP_DIR/MASTER_TPMCTX/EXTKEYSTORE
+fi
+
+# Service configuration
+cp ../../modules/dias-ltk-stk/config/master_kmngr.ini $CONFIG_PATH
+
+sudo cp ../services/dias-ltk-stk-service/$SERVICE_NAME /etc/systemd/system/
+
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl restart $SERVICE_NAME
