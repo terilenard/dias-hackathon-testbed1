7c4667d2c4e1f6fbf345071643d8b9b108fc50b2
Teri Lenard <78149766+terilenard@users.noreply.github.com>
2022-07-27 14:58:54 +0300

    Update logging.sh

--- toolchain/scripts/logging.sh
+++ toolchain/scripts/logging.sh
@@ -3,24 +3,22 @@
 # Create directories
 sudo mkdir -p /var/log/dias-logging/
 
-sudo mkdir -p /etc/dias-logging/bin/
+sudo mkdir -p /etc/tpm_handlers/dias-logging/
 
 sudo mkdir /usr/lib/python3/dist-packages/dias-logging
 
-# Log files
-sudo touch /var/log/dias-logging/tpm_logger.log
-
+# Log file
 sudo touch /var/log/dias-logging/info.log
 
 # Permissions
 sudo chown -R $USER:$USER /var/log/dias-logging/
 
-sudo chown -R $USER:$USER /etc/dias-logging/
+sudo chown -R $USER:$USER /etc/tpm_handlers/dias-logging/
 
 # Dependencies
 pip3 install blist paho-mqtt
 
-# Service setup + Bootstrap
+# Service setup
 sudo cp  ../services/dias-logging-service/dias-logging.service /etc/systemd/system/dias-logging.service
 
 sudo sed -i "/User=/c\User=$USER" /etc/systemd/system/dias-logging.service
@@ -29,12 +27,10 @@ sudo sed -i "/Group=/c\Group=$USER" /etc/systemd/system/dias-logging.service
 
 cd ../../modules/dias-logging/src/bootstrap
 
-python3 bootstrap_tpm.py --provision_generator --path /etc/dias-logging/bin --out_pub key.pub --out_priv key.priv
-
 cd ..
 
-sudo cp client_mqtt.py handlers.py  __init__.py  tpm_logger.py utils.py wrapper.py /usr/lib/python3/dist-packages/dias-logging
-
+sudo cp log.py client_mqtt.py handlers.py  __init__.py  tpm_logger.py utils.py wrapper.py /usr/lib/python3/dist-packages/dias-logging
+sudo sed -i "/handler =/c\handler = logging.FileHandler(\"/var/log/dias-logging/info.log\")" /usr/lib/python3/dist-packages/dias-logging/log.py
 sudo cp config.ini /etc/dias-logging/
 
 sudo systemctl enable dias-logging.service
